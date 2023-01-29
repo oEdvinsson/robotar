42d129ad2637a37a87780e0d75b4e3d6038960b4
Oscar Edvinsson <oscaredvinsson@gmail.com>
2023-01-29 10:08:28 +0100

    Initial commit

---
+++ src/containers/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import { robots } from '../robots';
+import './App.css';
+import Scroll from '../components/Scroll';
+//import { render } from '@testing-library/react';
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+        robots: robots,
+        searchField: ''
+    }
+    }
+
+    componentDidMount() {
+      fetch('https://jsonplaceholder.typicode.com/users')
+       .then(response=> response.json())
+       .then(users => {this.setState({ robots: users})});
+    }
+
+    onSearchChange = (event) => {
+      this.setState({ searchField: event.target.value })
+        
+    }
+
+    render() {
+      const { robots, searchField } = this.state;
+      const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchField.toLowerCase());
+      })
+      return !robots.length ?
+        <h1>Loading</h1> :
+        (
+        <div className='tc'>
+          <h1 className='f1'>Robofriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <Scroll>
+          <CardList robots={filteredRobots} />
+          </Scroll>
+          </div>
+      );
+    }
+  }
+
+
+export default App;
